5c47a0a63c8096035654b96ca7d9fbf2f35a2de2
Abdessamed Seddiki <102583087+AbdessamedSed@users.noreply.github.com>
2023-04-18 15:24:44 +0100

    push plus

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -4,17 +4,19 @@ from django.db.models import Q
 from django.http import HttpResponse
 from .models import Room, Topic, Message
 from django.contrib.auth.decorators import login_required
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
 # Create your v
 #rooms = [
 #   {'id': 1, 'name':'Lets learn python'},
 #    {'id': 2, 'name':'Design with me'},
 #    {'id': 3, 'name':'Frontend developers'},
 #]
+
 
 def loginPage(request):
     page = 'login'
@@ -154,3 +156,14 @@ def deleteMessage(request, pk):
         return redirect('home')
     return render(request, 'base/delete.html', {'obj' : message})
 
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance = user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile' , pk = user.id)
+    return render(request, 'base/update-user.html', {'form' : form})
